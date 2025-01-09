fca36fa3859ec97382d0f6d7d984a10c7c53fec0
aaguada <alex.aguada@snoopconsulting.com>
2025-01-09 14:44:01 -0300

    se agrega validaciones para region y de tarjeta

---
+++ zonaRefri-ecommerce/src/app/validators/zona-refri-validators.spec.ts
@@ -0,0 +1,7 @@
+import { ZonaRefriValidators } from './zona-refri-validators';
+
+describe('ZonaRefriValidators', () => {
+  it('should create an instance', () => {
+    expect(new ZonaRefriValidators()).toBeTruthy();
+  });
+});
